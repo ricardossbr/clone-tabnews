caa5d89a3d5c62c1cbf938de3b134807283f1cf2
Ricardo Soares <riicardo.soares@gmail.com>
2025-11-19 03:22:26 +0000

    add database.js and .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-var status = (resquest, response) => {
+import database from "../../../../infra/database.js";
+
+var status = async (resquest, response) => {
+  const result = await database.query("SELECT 1+1 as SOMA;");
+  console.log(result);
   response.status(200).json({ chave: "hello world!" });
 };
 
