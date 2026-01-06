c6a855b8aafe2c2e7fc6260c58fe7005ccfd90a6
Ricardo Soares <riicardo.soares@gmail.com>
2026-01-06 22:21:10 +0000

    adiciona endpoint de status com informações do banco de dados e atualiza teste correspondente

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,39 @@
 import database from "infra/database.js";
 
 var status = async (resquest, response) => {
-  const result = await database.query("SELECT 1+1 as SOMA;");
-  console.log(result);
-  response.status(200).json({ chave: "hello world!" });
+  const updateAt = new Date().toISOString();
+
+  const versionDatabase = await database.query("SHOW server_version;");
+  const dbVersion =
+    versionDatabase.rows && versionDatabase.rows[0].server_version;
+
+  const max_connections = await database.query("SHOW max_connections;");
+  const maxConnections =
+    max_connections.rows && max_connections.rows[0].max_connections;
+
+  // conta conexões para o banco atual
+  const connRes = await database.query(
+    "SELECT COUNT(*) AS total_connections FROM pg_stat_activity WHERE datname = current_database();",
+  );
+  const connections =
+    connRes.rows && connRes.rows[0]
+      ? Number(connRes.rows[0].total_connections)
+      : null;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependeces: {
+      database: {
+        version: dbVersion,
+        max_connections: database.maxConnections,
+        connections: connections,
+      },
+    },
+  });
+
+  console.log(
+    `[STATUS] ${updateAt} - DB Version: ${dbVersion} - Connections: ${connections} - Max Connections: ${maxConnections}`,
+  );
 };
 
 export default status;
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,9 @@
 test("GET to api/v1/status should be returned 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+  const updateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(responseBody.update_at);
+  expect(responseBody.dependeces.database.version).toEqual("16.0");
 });
